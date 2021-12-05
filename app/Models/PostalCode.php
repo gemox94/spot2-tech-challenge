ac9db932d1d4cc622b0bd37c4db5b44d6dfa7f61
Gerardo Moxca Ruiz <gerardo.mx.rz@gmail.com>
2021-12-05 15:52:50 -0600

    chore: set relationships into models

--- app/Models/PostalCode.php
+++ app/Models/PostalCode.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\State;
+
 class PostalCode extends Model
 {
     use HasFactory;
+
+    public function state() {
+        return $this->belongsTo(State::class);
+    }
+
 }
