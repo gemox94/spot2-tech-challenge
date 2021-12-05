b8ead7cfdb736e7124a1620734b7e6902c9635c5
Gerardo Moxca Ruiz <gerardo.mx.rz@gmail.com>
2021-12-05 16:20:08 -0600

    feat: list postal codes

--- app/Http/Controllers/API/v1/PostalCodeController.php
+++ app/Http/Controllers/API/v1/PostalCodeController.php
@@ -4,7 +4,21 @@ namespace App\Http\Controllers\API\v1;
 
 use App\Http\Controllers\Controller;
 
+use App\Http\Resources\PostalCodeResource;
+use App\Models\PostalCode;
+use Illuminate\Http\Request;
+
+
 class PostalCodeController extends Controller
 {
-    //
+    /**
+     * List postal codes
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function list(Request $request) {
+        $postalCodes = PostalCode::with('state')->orderBy('code')->paginate(15);
+        return PostalCodeResource::collection($postalCodes);
+    }
 }
